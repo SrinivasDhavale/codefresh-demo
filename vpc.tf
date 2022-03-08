dbdd876266713c48f4da58bd7ea0e231c3e73b2d
SrinivasDhavale <90887078+SrinivasDhavale@users.noreply.github.com>
2022-03-08 08:57:14 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "mydemoooooo"
+    key    = "myapp/dev/terraform.tfstate"
+    region = "ao-south-1"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
